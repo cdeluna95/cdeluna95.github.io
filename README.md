2b3ee3e8952135cc82b08669b46163618d6918d3
Christan <cdeluna@ggc.edu>
2016-10-03 16:07:26 +0000

    Initial commit of new revamp

--- README.md
+++
@@ -1,2 +0,0 @@
-# cdeluna95.github.io
-* Older site is in gh-pages2 branch
---
+++ function.js
@@ -0,0 +1,30 @@
+/* Scroll page function */
+$(function() {
+  $('a[href*="#"]:not([href="#myCarousel"])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 500);
+        return false;
+      }
+    }
+  });
+});
+
+/* Selected class function */
+$(document).ready(function() {
+  $('.navbar a').on("click", function() {
+    $(this).parent().siblings().find('a').removeClass('selected');
+    $(this).addClass('selected');
+  });
+});
+
+/* Scroll removes selected class function */
+$(document).ready(function() {
+  $('.contact-up a').on("click", function() {
+    $('.navbar a').parent().siblings().find('a').removeClass('selected');
+  });
+});
