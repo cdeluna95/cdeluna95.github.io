f0a56d2b4a2b60df44216f4b36de13648fcc56e9
Christan <cdeluna@ggc.edu>
2017-01-09 03:03:31 +0000

    Fix favicon and change quotes consistency

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,20 +1,20 @@
-'use strict';
+"use strict";
 
 /**
  * Toggle different navigation bar events
  */
 $(function() {
-  var burger = $('.nav-burger'),
-      sidebar = $('.nav-sidebar'),
-      li = $('.nav-li'),
-      main = $('#header, #projects, #gallery, #about, #contact, #footer');
+  var burger = $(".nav-burger"),
+      sidebar = $(".nav-sidebar"),
+      li = $(".nav-li"),
+      main = $("#header, #projects, #gallery, #about, #contact, #footer");
       
-  burger.on('click', function(e) {
+  burger.on("click", function(e) {
     e.preventDefault();
-    burger.toggleClass('shift slide');
-    sidebar.toggleClass('slide');
-    li.toggleClass('accelerate');
-    main.toggleClass('darken freeze');
+    burger.toggleClass("shift slide");
+    sidebar.toggleClass("slide");
+    li.toggleClass("accelerate");
+    main.toggleClass("darken freeze");
   });
 });
 
@@ -25,25 +25,25 @@ $(function() {
   $(window).scroll(function() {
     var scrollPos = $(window).scrollTop(),
         heightPos = $(window).height(),
-        bar = $('.nav-bar'),
-        arrow = $('.nav-arrow');
+        bar = $(".nav-bar"),
+        arrow = $(".nav-arrow");
         
     // Change color
     if(scrollPos > (heightPos * 0.95)) {
-      bar.addClass('blacken');
+      bar.addClass("blacken");
     }
     
     else {
-      bar.removeClass('blacken');
+      bar.removeClass("blacken");
     }
     
     // Change appearance
     if(scrollPos > (heightPos * 3.45)) {
-      arrow.addClass('appear');
+      arrow.addClass("appear");
     }
     
     else {
-      arrow.removeClass('appear');
+      arrow.removeClass("appear");
     }
   });
 });
@@ -52,9 +52,9 @@ $(function() {
  * Scroll smoothly after clicking navigation links
  */
 $(function() {
-  $('a[href^="#"]').on('click', function(e) {
-    var target = $($(this).attr('href')),
-        html = $('html, body');
+  $('a[href^="#"]').on("click", function(e) {
+    var target = $($(this).attr("href")),
+        html = $("html, body");
         
     if(target.length) {
       e.preventDefault();
